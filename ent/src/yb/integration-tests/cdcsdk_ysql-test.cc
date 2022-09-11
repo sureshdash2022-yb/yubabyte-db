6fad5d7e7b93163954f1e03b28c529dab21c3604
sureshdash2022-yb <sdash@yugabyte.com>
2022-09-11 10:03:32 +0530

    POC: DDL support reading schema from WAL log: lint fix

--- ent/src/yb/integration-tests/cdcsdk_ysql-test.cc
+++ ent/src/yb/integration-tests/cdcsdk_ysql-test.cc
@@ -4053,16 +4053,13 @@ TEST_F(CDCSDKYsqlTest, YB_DISABLE_TEST_IN_TSAN(TestCDCSDKMultipleDDLWithRestartT
   ASSERT_OK(WriteRows(
       6 /* start */, 10 /* end */, &test_cluster_, {kValue2ColumnName, kValue3ColumnName}));
 
-  // remove one column
-   table1 =
-      ASSERT_RESULT(DropColumn(&test_cluster_, kNamespaceName, kTableName, kValue2ColumnName));
-  ASSERT_OK(WriteRows(
-      11 /* start */, 15 /* end */, &test_cluster_));
+  // Remove one column
+  table1 = ASSERT_RESULT(DropColumn(&test_cluster_, kNamespaceName, kTableName, kValue2ColumnName));
+  ASSERT_OK(WriteRows(11 /* start */, 15 /* end */, &test_cluster_));
 
-  // add the 2 columns
+  // Add the 2 columns
   table1 = ASSERT_RESULT(AddColumn(&test_cluster_, kNamespaceName, kTableName, kValue4ColumnName));
-  ASSERT_OK(WriteRows(
-      16 /* start */, 20 /* end */, &test_cluster_));
+  ASSERT_OK(WriteRows(16 /* start */, 20 /* end */, &test_cluster_));
 
   CDCStreamId stream_id = ASSERT_RESULT(CreateDBStream(IMPLICIT));
   auto resp = ASSERT_RESULT(SetCDCCheckpoint(stream_id, tablets));
