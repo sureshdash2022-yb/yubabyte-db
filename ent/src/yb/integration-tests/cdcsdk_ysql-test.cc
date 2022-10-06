ac51665a93a9faef36aae97176419c6d21926a6e
sureshdash2022-yb <sdash@yugabyte.com>
2022-10-06 08:32:26 +0530

    alter table support with cdcsdk

--- ent/src/yb/integration-tests/cdcsdk_ysql-test.cc
+++ ent/src/yb/integration-tests/cdcsdk_ysql-test.cc
@@ -90,7 +90,7 @@ DECLARE_uint64(aborted_intent_cleanup_ms);
 DECLARE_int32(cdc_min_replicated_index_considered_stale_secs);
 DECLARE_int32(log_min_seconds_to_retain);
 DECLARE_int32(timestamp_history_retention_interval_sec);
-DECLARE_int32(cdc_history_retention_interval_sec);
+DECLARE_int32(cdc_syscatalog_history_retention_interval_sec);
 
 DECLARE_bool(enable_single_record_update);
 DECLARE_bool(enable_delete_truncate_cdcsdk_table);
