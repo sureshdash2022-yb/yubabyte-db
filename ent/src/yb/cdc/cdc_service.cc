b3c174f27a613bed2d14a8025d0e578bc3c474d1
sureshdash2022-yb <sdash@yugabyte.com>
2022-10-10 15:20:27 +0530

    14293:upgrading the universe from 2.15.3 to 2.15.4, causing intent Gced, review comments fix

--- ent/src/yb/cdc/cdc_service.cc
+++ ent/src/yb/cdc/cdc_service.cc
@@ -2023,9 +2023,10 @@ Result<TabletIdCDCCheckpointMap> CDCServiceImpl::PopulateTabletCheckPointInfo(
     if (record.source_type == CDCSDK) {
       // Support backward compatibility, where active_time as not part of cdc_state table.
       if (last_active_time_cdc_state_table == std::numeric_limits<int64_t>::min()) {
-        LOG(WARNING) << "In previous server version, active time was not part of cdc_state table,"
-                        "as part upgrade forcefully update active time for the tablet_id: "
-                     << tablet_id;
+        LOG(WARNING)
+            << "In previous server version, active time was not part of cdc_state table,"
+               "as a part of upgrade, updating the active time forcefully for the tablet_id: "
+            << tablet_id;
         last_active_time_cdc_state_table = GetCurrentTimeMicros();
       }
       auto session = client()->NewSession();
